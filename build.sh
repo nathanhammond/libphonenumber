b289d4eeae8042597d14818907624edeb97850c5
Seyi Adebajo <seyi.adebajo@uxpsystems.com>
2016-06-29 22:45:37 -0400

    Removes placeholder to build conf and guard in build script

--- build.sh
+++ build.sh
@@ -1,11 +1,5 @@
 #!/usr/bin/env bash
 
-`grep -e "/PATH/TO/ANT-CONTRIB.JAR" build.xml 2>&1 1>/dev/null`
-if [ $? -eq 0 ]; then
-  printf "You need to set the path to ant-contrib.jar in build.xml\nChange '/PATH/TO/ANT-CONTRIB.JAR' to match your system path to ant-contrib.jar\n"
-  exit 1
-fi
-
 if [ ! -d "vendor" ]; then
   mkdir vendor
 fi
