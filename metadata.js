72ad796fad9d335c15eb82b3cea53a33c879ef4c
Bruhn, Daniel <dbruhn@paypal.com>
2016-09-01 17:30:45 -0700

    For each region, include metadata of main country for calling code
    cf. #23

--- metadata.js
+++ metadata.js
@@ -22,7 +22,7 @@ function writeFile(regionCode, countryToMetadata, countryCodeToRegionCodeMap) {
   return fs.writeFileSync(outpath + 'metadata_'+regionCode+'.js', output.join(';\n'));
 }
 
-var regionCodeToCountryCodeMap = {};
+var regionCodeToCountryCodeMap = {}; // construct map from territory codes ('region code') to country calling codes ('country code')
 for (var countryCode in context.i18n.phonenumbers.metadata.countryCodeToRegionCodeMap) {
   context.i18n.phonenumbers.metadata.countryCodeToRegionCodeMap[countryCode].forEach(function(regionCode) {
     if (!regionCodeToCountryCodeMap[regionCode]) {
@@ -32,16 +32,30 @@ for (var countryCode in context.i18n.phonenumbers.metadata.countryCodeToRegionCo
   });
 }
 
-// Grab the appropriate data.
+// Grab the appropriate data, which includes the metadata for:
+//   - the region (e.g. CC)
+//   - the main country for the same calling code (e.g. AU, which shares calling code 61 with CC)
+//
+// Note: 'countryCode' = country calling code (e.g. 61 for AU)
+//       'regionCode' = ISO 3166-1 alpha-2 (e.g. AU)
+
 var countryToMetadata, countryCodeToRegionCodeMap;
 for (var regionCode in context.i18n.phonenumbers.metadata.countryToMetadata) {
   if (!Number.isNaN(parseInt(regionCode))) { continue; }
 
-  countryToMetadata = {};
-  countryCodeToRegionCodeMap = {};
+  var countryCode = regionCodeToCountryCodeMap[regionCode],
+     // get main country (first in the array) for the shared country calling code and include its metadata
+    mainCountryForCode = context.i18n.phonenumbers.metadata.countryCodeToRegionCodeMap[countryCode][0],
+    regionCodesToAdd = (regionCode === mainCountryForCode) ? [regionCode] : [mainCountryForCode, regionCode];
 
-  countryToMetadata[regionCode] = context.i18n.phonenumbers.metadata.countryToMetadata[regionCode];
-  countryCodeToRegionCodeMap[regionCodeToCountryCodeMap[regionCode]] = [regionCode];
+  countryCodeToRegionCodeMap = {};
+  countryCodeToRegionCodeMap[countryCode] = regionCodesToAdd; // populate country code to region code map
+
+  // add metadata for each relevant region code
+  countryToMetadata = {};
+  regionCodesToAdd.forEach(function (regionCodeToAdd) {
+    countryToMetadata[regionCodeToAdd] = context.i18n.phonenumbers.metadata.countryToMetadata[regionCodeToAdd];
+  });
 
   writeFile(regionCode, countryToMetadata, countryCodeToRegionCodeMap);
 }
